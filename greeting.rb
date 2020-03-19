bc389ce938e4a72e37cc05d717285a0bf209de6d
Daniel Kersten <dkersten1994@gmail.com>
2020-03-19 15:25:24 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,13 @@
+def greeting
+   puts "hello world"
+end
+
+def say_greeting_five_time
+   puts "hello"
+   puts "hello"
+   puts "hello"
+   puts "hello"
+   puts "hello"
+end
+
+say_greeting_five_time
